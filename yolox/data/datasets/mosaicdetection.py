6f9e6305222cc5697679baf5681804de6ed02e43
vk <v.kotteeswaran@uqu.do>
2025-03-07 14:46:44 +0530

    random sunflare rad change

--- yolox/data/datasets/mosaicdetection.py
+++ yolox/data/datasets/mosaicdetection.py
@@ -75,7 +75,7 @@ class MosaicDetection(Dataset):
         self.weather_transform = A.OneOf(
             [A.RandomRain(brightness_coefficient=0.9, drop_width=1, blur_value=5, p=1),
              A.RandomSnow(brightness_coeff=2.5, snow_point_lower=0.3, snow_point_upper=0.5, p=1),
-             A.RandomSunFlare(flare_roi=(0, 0, 1, 0.5), angle_lower=0.5, p=1),
+             A.RandomSunFlare(flare_roi=(0, 0, 1, 0.5), angle_lower=0.5, src_radius=100, p=1),
              A.RandomShadow(num_shadows_lower=1, num_shadows_upper=1, shadow_dimension=5, shadow_roi=(0, 0.5, 1, 1),p=1),
              A.RandomFog(fog_coef_lower=0.7, fog_coef_upper=0.8, alpha_coef=0.1, p=1)
              ], p=1.0
